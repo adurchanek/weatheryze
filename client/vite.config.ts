bda8540b9650f6b1711a5a2aada6ffd973ed72da
adurchanek <adurchanek@gmail.com>
2025-02-06 02:01:47 -0500

    Add non-code changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
   return {
     plugins: [react()],
     server: {
       proxy: {
         "/api": {
-          target: process.env.VITE_BACKEND_URL,
+          target: env.VITE_BACKEND_URL,
           changeOrigin: true,
-          secure: true, // Ensures HTTPS connections are used
+          secure: true,
         },
         "/socket.io": {
-          target: process.env.VITE_BACKEND_URL,
+          target: env.VITE_BACKEND_URL,
           ws: true,
           changeOrigin: true,
           secure: true,
